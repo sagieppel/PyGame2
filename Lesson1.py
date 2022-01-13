a24b79b249bf45242c3487b3d4ef913ece23bad2
sagieppel <sagieppel@gmail.com>
2022-01-13 16:54:59 -0500

    Update Lesson1.py

--- Lesson1.py
+++ Lesson1.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 
-screen=pygame.display.set_mode([500,500]) # create screen
+screen=pygame.display.set_mode([500,500]) 
 
 for i in range(100000):
    pygame.draw.circle(screen, [0,255,0], [200,200], 20)
