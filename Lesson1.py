62c4bc73e6b06bbcd0037a6d4c674739c72d6957
sagieppel <sagieppel@gmail.com>
2022-01-13 17:39:36 -0500

    Update Lesson1.py

--- Lesson1.py
+++ Lesson1.py
@@ -1,15 +1,17 @@
 import pygame 
+import random
+
 pygame.init()
 
 screenSize=600
 screen=pygame.display.set_mode([screenSize,screenSize]) 
 
 for i in range(100000000):
-   red=255
-   green=0
-   blue=100
-   x=200
-   y=200
-   scale=10
+   red=random.randint(0,255)
+   green=random.randint(0,255)
+   blue=random.randint(0,255)
+   x=random.randint(0,600)
+   y=random.randint(0,600)
+   scale=random.randint(0,55)
    pygame.draw.circle(screen, [red,green,blue], [x,y], scale)
    pygame.display.update()
