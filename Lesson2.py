c7d6f8322f98e268449359602e45b35c46d22784
sagieppel <sagieppel@gmail.com>
2022-01-20 17:51:31 -0500

    Update Lesson2.py

--- Lesson2.py
+++ Lesson2.py
@@ -12,13 +12,28 @@ def createCircle(x,y):
    
    circle['x']=x#+random.randint(-50,50)
    circle['y']=y#+random.randint(-50,50)
+  
+   circle["speedx"]=0.1
+   circle["speedy"]=0.1
    
+  
    circle["scale"]= 20# random.randint(0,5)
    #pygame.time.delay(5)
-   
-   pygame.draw.circle(screen, [circle["red"], circle["green"], circle["blue"]], [circle['x'],circle['y']], circle["scale"])
+   color = (255,0,0)
+   pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+  # pygame.draw.circle(screen, [circle["red"], circle["green"], circle["blue"]], [circle['x'],circle['y']], circle["scale"])
    pygame.display.update()
 ###############################################################################
+# def change(circle):
+#     circle['x']+=circle["speedx"]
+#     pygame.draw.circle(screen, [circle["red"], circle["green"], circle["blue"]], [circle['x'],circle['y']], circle["scale"])
+#     pygame.display.update()
+####################################################################  
+   
+
+
+
+
 
 pygame.init()
 
@@ -30,5 +45,3 @@ for i in range(100000000):
    pygame.event.get()
    x,y = pygame.mouse.get_pos()
    createCircle(x,y)
-   createCircle(100,100)
-   createCircle(300,300)
