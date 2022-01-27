799605432574832c4e04b9a2601e911e054f124a
sagieppel <sagieppel@gmail.com>
2022-01-27 17:55:26 -0500

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -10,12 +10,13 @@ def create(x,y):
   circle["size"]= random.randint(1,10)
   circle["xspeed"]=random.randint(-100,100)/1000
   circle["yspeed"]=random.randint(-100,100)/1000
+  circle["noise"]=0.01
   return circle
   
 #------------------Draw a circle--------------------------------------------------------
 def update(circle):
-    circle["x"] += circle["xspeed"]
-    circle["y"] += circle["yspeed"]
+    circle["x"] +=  circle["xspeed"]+random.randint(-100,100)*circle["noise"]
+    circle["y"] += circle["yspeed"]+random.randint(-100,100)*circle["noise"]
     pygame.draw.circle(screen, circle["color"], [int(circle["x"]),int(circle["y"])], int(circle["size"]))
 
 #----------------initiate pygame---------------------------------------------------------
@@ -24,7 +25,7 @@ screensize=700
 screen=pygame.display.set_mode([screensize,screensize])
 ar=[]
 for i in range(100):
-     circle = create(300, 400)
+     circle = create(200, 200)
      ar.append(circle)
 #----------------------------Main loop--------------------------------------------------------------------
 for i in range(1000000):
