31061be38e1b70f6af7ffb16cc3442dff5d2dd13
sagieppel <sagieppel@gmail.com>
2022-01-27 16:56:22 -0500

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -1,37 +1,33 @@
 import pygame
 import random
 #-----------------Create a circle at X,Y--------------------------------------------------------
-def createcircle(x,y):
+def create(x,y):
   circle={}
   circle["color"]=(random.randint(0,255),random.randint(0,255),random.randint(0,255))
   circle["x"]=x #random.randint(0,255)
   circle["y"]=y #random.randint(0,255)
-  circle["speedx"]= random.randint(-100,100)/100
-  circle["speedy"]= random.randint(-100,100)/100
-  circle["noise"]= 0.9 #random.randint(0,255)
-  circle["shrink"] = 0.99  # random.randint(0,255)
-  circle["gravity"] = 0.001
   circle["size"]=10#random.randint(10,100)
   return circle
 #------------------Draw a circle--------------------------------------------------------
-def drawcircle(circle):
-    circle["x"]+=circle["speedx"]+random.randint(-1,1)*circle["noise"]
-    circle["y"]+=circle["speedy"]+random.randint(-1,1)*circle["noise"]
-    circle["speedy"]+=circle["gravity"]
-    circle["size"]*= circle["shrink"]
+def update(circle):
     pygame.draw.circle(screen, circle["color"], [int(circle["x"]),int(circle["y"])], int(circle["size"]))
 #----------------initiate pygame---------------------------------------------------------
 pygame.init()
 screensize=500
 screen=pygame.display.set_mode([screensize,screensize])
-objs=[]
+
+circle = create(200, 200)
 #----------------------------Main loop--------------------------------------------------------------------
 for i in range(1000000):
-  pygame.event.get()
-  x,y = pygame.mouse.get_pos() # Get position of the mouse
-  obj = createcircle(x, y)
-  objs.append(obj)
-  pygame.Surface.fill(screen,(0,0,0))
-  for ob in objs:
-       drawcircle(ob)
-  pygame.display.update()
+    update(circle)
+    pygame.display.update()
+    
+  
+  
+  
+#   pygame.event.get()
+#   x,y = pygame.mouse.get_pos() # Get position of the mouse
+  
+#   pygame.Surface.fill(screen,(0,0,0))
+ 
+
