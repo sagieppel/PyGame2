40f03eea4a55d60f17744a9a59f9687eff095de6
sagieppel <sagieppel@gmail.com>
2022-03-17 17:31:51 -0400

    Update Lesson2.py

--- Lesson2.py
+++ Lesson2.py
@@ -11,11 +11,11 @@ circle["color"] = [0,0,255]
 circle['x'] = 200
 circle['y'] = 100
 circle["radius"] = 40
-circle["speedX"] = 1
+circle["speedX"] = 0.1
 circle["speedY"] = -1
 
 for i in range(1000000):
   screen.fill((0,0,0))
   circle['x'] = circle['x'] + circle["speedX"]
-  pygame.draw.circle(screen, circle["color"], [int(circle['x']),int(circle['y']), int(circle["radius"]))
+  pygame.draw.circle(screen, circle["color"], [int(circle['x']),int(circle['y'])], int(circle["radius"]))
   pygame.display.update()
