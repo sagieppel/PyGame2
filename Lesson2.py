7f704e120b42cb8eddbd8dd3034790c32782e6c0
sagieppel <sagieppel@gmail.com>
2022-03-17 17:27:29 -0400

    Update Lesson2.py

--- Lesson2.py
+++ Lesson2.py
@@ -8,10 +8,14 @@ screen=pygame.display.set_mode([screensize,screensize]) #
 
 circle = {}
 circle["color"] = [0,0,255]
-circle["x"] = 100
-circle["y"] = 100
+circle['x'] = 200
+circle['y'] = 100
 circle["radius"] = 40
+circle["speedX"] = 1
+circle["speedY"] = -1
 
 for i in range(1000000):
-  pygame.draw.circle(screen, circle["color"], [circle["x"],circle["y"]], circle["radius"])
+  screen.fill((0,0,0))
+  circle['x'] = circle['x'] + circle["speedX"]
+  pygame.draw.circle(screen, circle["color"], [circle['x'],circle['y']], circle["radius"])
   pygame.display.update()
